3627504b98ee5703a5758c9af09ff7a30da1bed2
programminghour <44949140+programminghour@users.noreply.github.com>
2019-03-22 09:10:58 +0100

    Create stosik.h

---
+++ stosik.h
@@ -0,0 +1,18 @@
+
+struct element{
+    public:
+        float dana;
+        element *nast;
+
+};
+
+class stos {
+  protected:
+       element *pierwszy;
+  public:
+         stos();
+    void dodaj(float x);
+    void wypisz();
+    void usun();
+
+};
